144a0a389cf58ad3c2c68725699bf9239192dd37
NIRANJANRAJ S <niranjanraj699@gmail.com>
2026-03-02 13:59:08 +0530

    UC1: Print OOPS to console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,15 @@
-public class OOPSBannerApp{
-	public static void main(String[] args) {
-		System.out.println("OOPS");
-	}
+/**
+ * OOPSBannerApp
+ * Prints OOPS to console
+ * @author Niranjanraj
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
 }
