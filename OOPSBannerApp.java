22ca97078c4602bfe61ea19e068d14f24f5ded25
NIRANJANRAJ S <niranjanraj699@gmail.com>
2026-03-02 14:29:37 +0530

    UC2: Print OOPS banner using ASCII art

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,22 @@
 /**
  * OOPSBannerApp
- * Prints OOPS to console
+ * Prints OOPS in large banner format
  * @author Niranjanraj
- * @version 1.0
+ * @version 2.0
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        System.out.println("*****   *****   *****   *****");
+        System.out.println("*   *   *   *   *   *   *    ");
+        System.out.println("*   *   *   *   *   *   *    ");
+        System.out.println("*   *   *   *   *****   *****");
+        System.out.println("*   *   *   *   *       *    ");
+        System.out.println("*   *   *   *   *       *    ");
+        System.out.println("*****   *****   *       *****");
 
     }
+
 }
